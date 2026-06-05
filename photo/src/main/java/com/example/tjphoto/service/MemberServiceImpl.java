d8f27491db887113f772430b6cf6addf7fe8212b
김태연 <kty51713@gmail.com>
2026-06-05 19:24:08 +0900

    ㅇㅇ

--- photo/src/main/java/com/example/tjphoto/service/MemberServiceImpl.java
+++ photo/src/main/java/com/example/tjphoto/service/MemberServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.tjphoto.service;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
